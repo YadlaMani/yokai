c00fa0b48b78f2eea0e2e0516a1d31c5cc40ff4f
YadlaMani <yadlamaniymn2005@gmail.com>
2025-10-05 22:01:36 +0530

    added database

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import { Telegraf } from "telegraf";
 import { message } from "telegraf/filters";
-
+import { PrismaClient } from "./generated/prisma";
 const bot = new Telegraf(process.env.BOT_TOKEN!);
 
 bot.start((ctx) => {
