ee2bf055bd6c3c5306ff2d8007e104668fc6d89d
YadlaMani <yadlamaniymn2005@gmail.com>
2025-10-05 22:25:52 +0530

    added dummy port for deployment

--- index.ts
+++ index.ts
@@ -1,9 +1,17 @@
 import { Telegraf } from "telegraf";
 import { message } from "telegraf/filters";
+import { serve } from "bun";
 import { PrismaClient } from "./generated/prisma";
 const bot = new Telegraf(process.env.BOT_TOKEN!);
 
 bot.start((ctx) => {
+  console.log(ctx.from);
   ctx.reply("Yo! welcome");
 });
 bot.launch();
+serve({
+  port: process.env.PORT || 3000,
+  fetch() {
+    return new Response("Bot is running ✅");
+  },
+});
